cefd5e464de37021e1ce6cbaa956c46b2fee5436
tim mårtensson <tim.martensson92@gmail.com>
2024-05-28 15:49:21 +0200

    cleaner complete

---
+++ CourseMessageProvider/Functions/RequestCleaner.cs
@@ -0,0 +1,27 @@
+using System;
+using CourseMessageProvider.Data.Contexts;
+using CourseMessageProvider.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace CourseMessageProvider.Functions
+{
+    public class RequestCleaner(ILogger<RequestCleaner> logger, CleanerService cleanerService)
+    {
+        private readonly ILogger<RequestCleaner> _logger = logger;
+        private readonly CleanerService _cleanerService = cleanerService;
+
+        [Function("RequestCleaner")]
+        public async void Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer)
+        {
+            try
+            {
+                await _cleanerService.RemoveRequestsAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR : RequestCleaner.Run() :: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CourseMessageProvider/Services/CleanerService.cs
@@ -0,0 +1,24 @@
+﻿using CourseMessageProvider.Data.Contexts;
+using CourseMessageProvider.Functions;
+using Microsoft.Extensions.Logging;
+
+namespace CourseMessageProvider.Services;
+
+public class CleanerService(ILogger<CleanerService> logger, DataContext context)
+{
+    private readonly ILogger<CleanerService> _logger = logger;
+    private readonly DataContext _context = context;
+
+    public async Task RemoveRequestsAsync()
+    {
+        try
+        {
+            _context.JoinCourseRequests.RemoveRange(_context.JoinCourseRequests);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : CleanerService.RemoveRequestsAsync() :: {ex.Message}");
+        }
+    }
+}
